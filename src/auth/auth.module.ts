455da3f83251bfd09ee44b9bdb58df0c55768740
IgnaDev <arcortoon@gmail.com>
2024-01-03 18:52:45 -0300

    feat: Implementa lógica inicial de JWT
    Se ha aplicado la lógica inicial para el uso de JSON Web Tokens (JWT). Ahora es necesario configurar una variable de entorno llamada JWT_SECRET que contendrá la palabra secreta necesaria para la generación y verificación de tokens.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [PassportModule],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '20s' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   exports: [AuthService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
+dotenv.config();
+
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
