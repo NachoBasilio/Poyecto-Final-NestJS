aeb8a357430229fd4d70a2d778e86849d3a028e6
IgnaDev <arcortoon@gmail.com>
2024-01-06 19:30:50 -0300

    feat(posts): Agrega controlador y servicio para gestionar posts
    Se añadió un controlador y un servicio para la creación y obtención de posts. La creación asigna automáticamente el nombre de usuario del autor. La autenticación está garantizada mediante la guardia .

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,16 +1,30 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Req,
+  UseGuards,
+  Query,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { PostsService } from './posts.service';
+import { PostDocument } from '../models/post.model';
 
 @Controller('posts')
 @UseGuards(JwtAuthGuard)
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @Post()
+  async create(@Body() post: PostDocument, @Req() req) {
+    return this.postsService.create(post, req.user);
+  }
+
   @Get()
-  getProtectedData() {
-    return {
-      posts: ['Buenas'],
-    };
+  async findAll(@Query('limit') limit: number, @Query('page') page: number) {
+    limit = limit > 0 ? limit : 10;
+    page = page > 0 ? page : 1;
+    return this.postsService.findAll(limit, page);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { PostDocument, PostSchema } from '../models/post.model';
+import { PostDocument } from '../models/post.model';
+import { UserDocument } from 'src/models';
 
 @Injectable()
 export class PostsService {
@@ -9,4 +10,19 @@ export class PostsService {
     @InjectModel('Post')
     private readonly postModel: Model<PostDocument>,
   ) {}
+
+  // posts.service.ts
+  async create(post: PostDocument, user: UserDocument): Promise<PostDocument> {
+    post.author = user.username;
+    const createdPost = new this.postModel(post);
+    return createdPost.save();
+  }
+
+  async findAll(limit = 10, page = 1): Promise<PostDocument[]> {
+    return this.postModel
+      .find()
+      .limit(limit)
+      .skip((page - 1) * limit)
+      .exec();
+  }
 }
